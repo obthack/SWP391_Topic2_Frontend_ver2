8ba485d96e2c348cbc3ac5075883d576b1781918
Tan <ptdtan43@gmail.com>
2025-10-19 15:22:06 +0700

    none

--- backend/obj/Debug/net8.0/EVTB_Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/EVTB_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVTB_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c1997e9e5a0c218ee4d285f5164b9650c18938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce597265d4b84bb640472282fce80efc490c9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVTB_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVTB_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
