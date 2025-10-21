d10bd89fe7bd6bafb844dcc572af54633215ce51
obthack <thach2548@gmail.com>
2025-10-22 01:03:17 +0700

    Revert "Merge pull request #19 from obthack/xiaobo"
    This reverts commit 77921ad2e08ce465f33fd17e056fd1d20e254343, reversing
    changes made to 9d6019f0fc96ecc80642bdb36cb120c7c25ba8a0.

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom']
-        }
-      }
-    }
-  },
   server: {
     proxy: {
       '/api': {
-        target: 'https://ev-and-battery-trading-platform-be.onrender.com',
+        target: 'http://localhost:5044', // địa chỉ backend
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
   },
